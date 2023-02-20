ca20818bb9087122a79885be6ba3bef14ef1f0f3
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-02-20 17:29:50 +0500

    infinity scroll duplicate key results error Fixed

--- src/components/News.js
+++ src/components/News.js
@@ -23,18 +23,17 @@ export class News extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // articles: this.articles, //to load articles  from above array
       articles: [],
       loading: true,
       page: 1,
-      totalResults: 0,
-    };
+      totalResults: 0
+    }
     document.title = `${this.capitalizeFirstLetter(
       this.props.category
     )} - NewsMonkey`;
   }
 
-  async updateNews(pageNo) {
+  async updateNews() {
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=405dd9e5508b412b911829a863abdb46&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -42,7 +41,7 @@ export class News extends Component {
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
-      loading: false,
+      loading: false
     });
   }
 
@@ -50,25 +49,25 @@ export class News extends Component {
     this.updateNews();
   }
 
-  // handlePrevClick = async () => {
-  //   this.setState({ page: this.state.page - 1 });
-  //   this.updateNews();
-  // };
+  handlePrevClick = async () => {
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
 
-  // handleNextClick = async () => {
-  //   this.setState({ page: this.state.page + 1 });
-  //   this.updateNews();
-  // };
+  handleNextClick = async () => {
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
 
   fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=405dd9e5508b412b911829a863abdb46&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ page: this.state.page + 1 })
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=405dd9e5508b412b911829a863abdb46&page=${this.state.page + 1 }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
-    let parsedData = await data.json();
+    let parsedData = await data.json()
     this.setState({
       articles: this.state.articles.concat(parsedData.articles),
       totalResults: parsedData.totalResults
-    });
+    })
   };
 
   render() {
@@ -89,10 +88,8 @@ export class News extends Component {
         
         <div className="container">
             <div className="row">
-              {/* { !this.state.loading && this.state.articles.map((element) => { */}
               {this.state.articles.map((element) => {
-                return (
-                  <div className="col-md-4" key={element.url}>
+                return <div className="col-md-4" key={element.url}>
                     <NewsItem
                       title={element.title ? element.title : ""}
                       description={element.description ? element.description : ""}
@@ -103,32 +100,10 @@ export class News extends Component {
                       source={element.source.name}
                     />
                   </div>
-                );
               })}
             </div>
           </div>
         </InfiniteScroll>
-        {/* <div className="container d-flex justify-content-between">
-          <button
-            disabled={this.state.page <= 1}
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handlePrevClick}
-          >
-            &larr; Previous
-          </button>
-          <button
-            disabled={
-              this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
-            }
-            type="button"
-            className="btn btn-dark"
-            onClick={this.handleNextClick}
-          >
-            Next &rarr;
-          </button>
-        </div> */}
       </>
     );
   }
