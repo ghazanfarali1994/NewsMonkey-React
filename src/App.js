e6756d9bc58b021a4f1f1b2cee4b6034d60876f9
ghazanfarali1994 <ghazanfar006@gmail.com>
2023-06-07 17:10:06 +0500

    Class Components to Function Components Changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 import { Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
 
-export default class App extends Component {
-  pageSize = 15;
+const App = () => {
+  const pageSize = 15;
   // apiKey= process.env.REACT_APP_NEWS_API   /* Use if you want to Hide NEWS API Key in .env.local file */
-  apiKey= "405dd9e5508b412b911829a863abdb46" /* if you want to use Hard Code NEWS API Key Here */
+  const apiKey= "405dd9e5508b412b911829a863abdb46" /* if you want to use Hard Code NEWS API Key Here */
 
-  state = {
-    progress: 0
-  }
 
-  setProgress = (progress) => {
-    this.setState({progress: progress})
-  }
+  const [progress, setProgress] = useState(0)
 
-  render() {
+
     return (
       <>
         <NavBar />
         <LoadingBar
           height={3}
           color='#f11946'
-          progress={this.state.progress}
+          progress={progress}
         />
         <Routes>
-          <Route path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" />} />
-          <Route path="business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business" />} />
-          <Route path="entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
-          <Route path="general" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general" />} />
-          <Route path="health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health" />} />
-          <Route path="science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science" />} />
-          <Route path="sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
-          <Route path="technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="technology" />} />
+          <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route path="business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
+          <Route path="entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment" />} />
+          <Route path="general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+          <Route path="health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+          <Route path="science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
+          <Route path="sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
+          <Route path="technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
         </Routes>
 
       </>
     )
-  }
 }
+
+export default App;
